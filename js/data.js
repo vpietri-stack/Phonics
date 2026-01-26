8b0acb2d831e56cead97fe36bdd94c3e84ebf245
vpietri-stack <v.pietri@esc-montpellier.com>
2026-01-26 12:33:10 +0800

    original commit

---
+++ js/data.js
@@ -0,0 +1,36 @@
+window.CURRICULUM = {
+    "寒假_CVCE_1630_1800": {
+        label: "寒假班 CVCE 16-30-18：00",
+        content: [
+            "The man has a cane.",
+            "The cat has a name.",
+            "The lad has a cake.",
+            "The dad has a rake.",
+            "The bag has tape.",
+            "The rat has a cage.",
+            "The cat is on the gate.",
+            "The map is on the case.",
+            "The cap is on a rake.",
+            "The bag is on the gate.",
+            "The fan is on a base.",
+            "The bat is on the gate.",
+            "A rat is on the cake.",
+            "A cap is on the cane.",
+            "A map is on the gate.",
+            "A cat is on the lane.",
+            "A bag is on the base.",
+            "The bat is in the cave.",
+            "The rat is in the cage.",
+            "The jam is in the vase.",
+            "The man is in the lane.",
+            "The map is in a case.",
+            "The pan is in the lake.",
+            "The lad is in the cave.",
+            "A cat is in the lane.",
+            "A rat is in the vase.",
+            "A fan is in the cave.",
+            "A bat is in the cage.",
+            "A cap is in the lake."
+        ]
+    }
+};
